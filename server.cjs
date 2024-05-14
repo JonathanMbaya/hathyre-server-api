eb4df633e43ed0b9ff2f6be2c191eb5c10774a74
unknown <jonathanmbaya13@gmail.com>
2024-05-14 02:50:50 +0200

    Mise en place du mode de paiement Stripe

--- server.cjs
+++ server.cjs
@@ -1,18 +1,15 @@
 const express = require("express");
 const cors = require('cors');
 const connectDB = require("./config/db.cjs");
-// const dotenv = require("dotenv").config();
-// const multer = require('multer');
-// const path = require('path');
+const stripe = require("stripe")('sk_test_51PFebILEHh2o4Mgieyrcbf461euTJRaK3DdRFzLWfQ88rnCpRaJmYx3MUOhhNQAoXLBesgL5uQGqnys9FJsYbTVP00W4HbXqym');
 const port = process.env.PORT || 8080;
 
-
-// connexion à la DB
+// Connexion à la base de données
 connectDB();
 
 const app = express();
 
-// Utilisez le middleware cors
+// Utilisez le middleware cors globalement
 app.use(cors());
 
 // Middleware qui permet de traiter les données de la Request
@@ -21,6 +18,33 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use("/api", require("./routes/product.routes.cjs"));
 app.use("/api", require("./routes/user.routes.cjs"));
+app.post("/stripe/load", async (req, res) => {
+    let { amount, id } = req.body;
+    console.log("amount & id :", amount, id);
+    try {
+        const paymentIntent = await stripe.paymentIntents.create({
+            amount: amount * 100, // Convertir le montant en centimes
+            currency: "EUR",
+            description: "HATHYRE COSMETICS",
+            payment_method: id,
+            confirm: true,
+            return_url: "https://hathyre-web.onrender.com/", // URL de retour après le paiement
+            automatic_payment_methods: {
+                enabled: true,
+            },
+        });
+
+        res.status(200).json({
+            message: "Paiement réussi",
+            success: true,
+        });
+    } catch (error) {
+        console.error("Erreur lors de la création du paiement :", error);
+        res.status(500).json({ error: "Erreur lors de la création du paiement" });
+    }
+});
+
+
 
 // mongodb+srv://jonathanmbaya13:hathyre1234@app-hathyre.grr5ukx.mongodb.net/
   
